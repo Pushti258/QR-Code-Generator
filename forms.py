8f20b24cd8beb40a1be7d2b6799046c931977746
Pushti258 <pushti393@gmail.com>
2026-07-11 16:24:19 +0530

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+class QRCodeForm(forms.Form):
+    restaurant_name = forms.CharField(
+        max_length=50,
+        label="Restaurant Name",
+        widget=forms.TextInput(attrs={
+         'class' :'form-control',
+        'placeholder':"Enter resturant name"   
+        })
+        
+        )
+    url = forms.URLField(
+        max_length=200,
+        label="Menue URL",
+        widget=forms.URLInput(attrs={
+            'class':'form-control',
+            'placeholder':"Enter the url for your online menue"
+        })
+        )
